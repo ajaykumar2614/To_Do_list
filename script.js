bce204801596539e01e4f82e8150ce81bc906e66
ajaykumar2614 <34391827+ajaykumar2614@users.noreply.github.com>
2018-08-27 19:29:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+window.onload = function () {
+    var input = document.getElementById('input-item');
+    var addButton = document.getElementById('add-item');
+    var list = document.getElementById('list');
+    var deleteChecked = document.getElementById('delete-checked');
+    var deleteThese = [];
+
+    deleteChecked.onclick = function () {
+        for ( i = 0 ;i < deleteThese.length ;i++){
+            deleteThese[i].remove();
+        }
+        console.log(deleteThese);
+        deleteThese = [];
+    }
+
+    window.delItem = function (element) {
+        console.log(element.parentElement.remove());
+    }
+
+    window.strikeThis = function (element) {
+        console.log(element.parentElement.children[0]);
+
+        if(element.checked === true){
+            element.parentElement.children[0].style.textDecoration = "line-through";
+            deleteThese.push(element.parentElement)
+
+        }else{
+            element.parentElement.children[0].style.textDecoration = "";
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+    function addListItem() {
+        var val = input.value;
+        var listElement = document.createElement('li');
+        listElement.innerHTML =
+            `
+                <span>${val}</span>
+                <input onclick="strikeThis(this)" type="checkbox" id="is-checked">
+                <button id="del-this-item" onclick="delItem(this)">X</button>
+            `
+        list.appendChild(listElement);
+        input.value = "";
+    }
+
+    addButton.onclick = function () {
+        addListItem();
+    }
+
+    input.addEventListener('keyup', function (event) {
+        // console.log(event.keyCode);
+        if(event.keyCode === 13){
+            addListItem();
+        }
+    })
+
+
+
+
+}
